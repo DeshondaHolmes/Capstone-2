a062b242bb256067c6e8a6930d77466c8e40d4b4
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-05-21 16:01:26 -0400

    Add navbar with links add radio buttons to select

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const locationRadioBtn = document.getElementById("locationRadioBtn");
+
+const parkTypeRadioBtn = document.getElementById("parkTypeRadioBtn");
+
+const locationSelect = document.getElementById("locationSelect");
+
+
+window.onload = function () {
+    locationRadioBtn.onclick = showLocations
+    console.log("radio click");
+};
+
+function showLocations() {
+   let stateOrTerritory = locationsArray.length;
+    if (locationRadioBtn.checked) {
+        console.log("show");
+        locationSelect.style.display = showLocations;
+    }
+
+}
+
